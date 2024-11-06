35e5271ff1c180f68e65d7ca8949d7a9eadd9f4d
amarallorena <159277845+amarallorena@users.noreply.github.com>
2024-11-06 11:19:46 -0300

    arena dos herois

---
+++ Arqueiro.java
@@ -0,0 +1,15 @@
+package ArenaDosHerois;
+
+public class Arqueiro extends Personagem{
+	
+	public Arqueiro(String nome) {
+		super(nome, 80, 20);
+	}
+	
+	@Override
+	public void atacar(Personagem oponente) {
+		oponente.receberDano(this.getDanoBase());
+		System.out.println(getNome()+
+				"disparou uma flecha em " + oponente.getNome() + "à distancia");
+	}
+}
